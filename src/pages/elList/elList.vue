429df59f3a2953f27dd76a4712f9582ca8b7ddeb
lumy <412005874@qq.com>
2021-04-27 10:30:49 +0800

    测试

---
+++ src/pages/elList/elList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-table :data='res.records'>
+            <el-button>hahah</el-button>
+        </el-table>
+    </div>
+</template>
+<script>
+const data = require('../../json/tabs.json')
+export default {
+    data(){
+        return {
+            res:data.content
+        }
+    },
+    mounted(){
+        console.log(this.res)
+    },
+    methods:{
+        
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
